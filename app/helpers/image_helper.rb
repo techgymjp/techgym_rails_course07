f3a04f6767ae942d2b5c18d4cfddc02b4e62d66a
konyaru <orira4666@gmail.com>
2021-09-23 03:24:10 +0900

    画像のトリミング機能を追加

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,3 +1,21 @@
 module ImageHelper
   IMAGE_SIZES = { small: [48, 36], medium: [164, 123], large: [600, 450] }
+
+  def resized_image_tag(image, size_name: :large)
+    options = {
+      size: resized_image_size(size_name: size_name),
+      class: 'image-size rounded',
+    }
+    return image_tag "no_images/#{size_name}.png", options unless image.attached?
+
+    combine_options = { gravity: :center, resize: "#{options[:size]}^", crop: "#{options[:size]}+0+0" }
+    image_tag image.variant(combine_options: combine_options).processed, options
+  end
+
+  private
+
+  def resized_image_size(size_name: :large)
+    width, height = IMAGE_SIZES[size_name]
+    "#{width}x#{height}"
+  end
 end
