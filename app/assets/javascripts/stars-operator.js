d0b211210018067b8ae07938d56b52d4766402f2
konyaru <orira4666@gmail.com>
2021-10-29 19:35:42 +0900

    JQueryリファクタリング

--- app/assets/javascripts/stars-operator.js
+++ app/assets/javascripts/stars-operator.js
@@ -1,34 +1,34 @@
 $(function() {
-  const TOTAL_STARS = 5;
-  const stars = $(".rating-stars");
-  const hidden_field = $("#review_star");
+  var TOTAL_STARS = 5;
+  var stars = $(".rating-star");
+  var hidden_field = $("#evaluation_star");
 
   function changeStarsCSSClass (star_num, rm_class, add_class) {
-    for(i = 0; i < star_num; i++) {
+    for(var i = 0; i < star_num; i++) {
       $(`#star_${i}`).removeClass(rm_class);
       $(`#star_${i}`).addClass(add_class);
     };
   };
 
-  function paintStars (rate_num) {
+  function changeStarsColor (rate) {
     changeStarsCSSClass(TOTAL_STARS, "fas fa-star", "far fa-star");
-    changeStarsCSSClass(rate_num, "far fa-star", "fas fa-star");
+    changeStarsCSSClass(rate, "far fa-star", "fas fa-star");
   };
 
   stars.mouseover(function () { 
-    const rate_num = Number($(this).attr('id').match(/\d/i)[0]) + 1;
+    var rate = Number($(this).attr('id').match(/\d/i)[0]) + 1;
 
-    paintStars(rate_num);
+    changeStarsColor(rate);
   });
 
   stars.click(function () { 
-    const rate_num = Number($(this).attr('id').match(/\d/i)[0]) + 1;
+    var rate = Number($(this).attr('id').match(/\d/i)[0]) + 1;
 
-    paintStars(rate_num);
-    hidden_field.val(rate_num);
+    changeStarsColor(rate);
+    hidden_field.val(rate);
   });
 
   stars.mouseout(function () { 
-    paintStars(hidden_field.val());
+    changeStarsColor(hidden_field.val());
   });
 });
